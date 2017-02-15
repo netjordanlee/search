9d185a3bff136d56dfbc9756bed2cf3b7c2d2e61
Laith Serhan <laithserhan@users.noreply.github.com>
2017-02-15 12:40:48 +1100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,30 @@
 
+// Version 20170215.1
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('hcis-search').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './style.js',
+        './plugins.js',
+        './main.js',
+        './db.xml'
+      ])
+      .then(() => self.skipWaiting());
+    })
+  )
+});
+
+self.addEventListener('activate',  event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
