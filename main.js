f23f26a1739569493b72f3f894351f5742e17950
Laith Serhan <laithserhan@users.noreply.github.com>
2017-05-10 13:42:26 +1000

    if find url exists, trigger search

--- main.js
+++ main.js
@@ -54,7 +54,8 @@ $(document).ready(function() {
   displayLoading(true);
 
   fetchDatabase();
-
+  readURLFind();
+  
   $("body").delay(666).fadeIn(333);
 
   $('.search').on('keyup', function(e) {
@@ -87,6 +88,28 @@ $(document).ready(function() {
   displayLoading(false);
 });
 
+function readURLFind() {
+  var qs = (function(a) {
+    if (a == "") return {};
+    var b = {};
+    for (var i = 0; i < a.length; ++i)
+    {
+      var p=a[i].split('=', 2);
+      if (p.length == 1)
+        b[p[0]] = "";
+      else
+        b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+    }
+    return b;
+  })(window.location.search.substr(1).split('&'));
+
+  if (qs["find"] != null) {
+    clearTimeout(timer);
+    $('.search').val(qs["find"]);
+    displayLoading(true);
+    timer = setTimeout("search($('.search').val())", 666);
+  }
+};
 
 function fetchDatabase() {
   var xml;
