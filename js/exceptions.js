74d177ca695ac6b62878577fffdd2c90acc95b2c
Laith Serhan <laithserhan@users.noreply.github.com>
2018-03-01 08:14:53 +1100

    Add files via upload
    organised search app file structure, search app settings and cache/manifest files remain in root dir to work

---
+++ js/exceptions.js
@@ -0,0 +1,41 @@
+function Exception(message) {
+	this.name = "Exception";
+	this.message = message ? (this.name + ": " + message) : (this.name + ": Unspecified exception.")
+	this.stack = (new Error().stack);
+}
+Exception.prototype = Error.prototype;
+
+function BadQueryException(message) {
+	this.name = "BadQueryException";
+	this.message = message ? (this.name + ": " + message) : (this.name + ": The query string was rejected for an unspecified reason.");
+	this.stack = (new Error().stack);
+}
+BadQueryException.prototype = Error.prototype;
+
+function NullResultsException(message) {
+	this.name = "NullResultsException";
+	this.message = message ? (this.name + ": " + message) : (this.name + ": The query returned no results.");
+	this.stack = (new Error().stack);
+}
+NullResultsException.prototype = Error.prototype;
+
+function ResultsPageOutOfBoundsException(message) {
+	this.name = "ResultsPageOutOfBoundsException";
+	this.message = message ? (this.name + ": " + message) : (this.name + ": Attempted to access a page of results that doesn't exist.");
+	this.stack = (new Error().stack);
+}
+ResultsPageOutOfBoundsException.prototype = Error.prototype;
+
+function XHRNotSupportedException(message) {
+	this.name = "XHRNotSupportedException";
+	this.message = message ? (this.name + ": " + message) : (this.name + ": This browser does not support XMLHTTPRequests.")
+	this.stack = (new Error().stack);
+}
+XHRNotSupportedException.prototype = Error.prototype;
+
+function XHRException(message) {
+	this.name = "XHRException";
+	this.message = message ? (this.name + ": " + message) : (this.name + ": The XMLHTTPRequest retuend an error.")
+	this.stack = (new Error().stack);
+}
+XHRException.prototype = Error.prototype;
