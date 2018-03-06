6e05191792b0672a09661cb048e447c649641f37
Laith Serhan <laithserhan@users.noreply.github.com>
2018-03-06 15:33:15 +1100

    Delete chrome.bat

--- chrome.bat
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-START "" "C:\Users\60078673\AppData\Local\Google\Chrome\Application\chrome.exe" --allow-file-access-from-files --disable-web-security --user-data-dir="%appdata%/chome_insecure/"
