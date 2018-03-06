6119249aa31af1d103191372e1ad60da8a4f4d31
Laith Serhan <laithserhan@users.noreply.github.com>
2018-03-06 14:58:11 +1100

    Add files via upload

---
+++ chrome.bat
@@ -0,0 +1,2 @@
+@ECHO OFF
+START "" "C:\Users\60078673\AppData\Local\Google\Chrome\Application\chrome.exe" --allow-file-access-from-files --disable-web-security --user-data-dir="%appdata%/chome_insecure/"
